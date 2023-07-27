e5458e7e4f21f9beef74fe2b5336307eee9bc8e8
Beatriz <beatrizdeferr@gmail.com>
2023-07-27 13:34:33 +0100

    Initial Commit

---
+++ aboutMe/aboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeApp.swift
+//  aboutMe
+//
+//  Created by scholar on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
