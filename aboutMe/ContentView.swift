23701965bf94c3f4408567cedc3299488c0aeaa4
Beatriz <beatrizdeferr@gmail.com>
2023-08-01 09:30:45 +0100

    Added comment

--- aboutMe/ContentView.swift
+++ aboutMe/ContentView.swift
@@ -6,21 +6,62 @@
 //
 
 import SwiftUI
+//COMMENT
 
 struct ContentView: View {
+    @State private var
+    showingPopover = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color(.systemBlue)
+                .ignoresSafeArea()
+            VStack {
+                Text("Beatriz")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.blue)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 500, height: 50)
+                    .background(Rectangle().fill(Color.white).shadow(radius: 3))
+                    .border(Color.gray)
+                Image("ducky")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .border(Color.gray)
+                
+                Spacer()
+                    .frame(width: 50)
+                    .frame(height: 300)
+                
+                Button("About Me"){
+                    
+                }
+                
+                
+                Button("Show Menu") {
+                    showingPopover = true
+                }
+                .popover(isPresented: $showingPopover) {
+                    Text("Your content here")
+                        .font(.headline)
+                        .padding()
+                }
+            }
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
